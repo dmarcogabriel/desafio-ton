2a643f972d0c8245191afd3d2e620aa7e5427b4a
Gabriel <lino.dmarco@gmail.com>
2021-12-08 20:24:00 -0300

    feat: start app code

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+const {defaults: tsjPreset} = require('ts-jest/presets');
+
+module.exports = {
+  rootDir: './',
+  preset: 'react-native',
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.spec.json',
+    },
+  },
+  // transform: {
+  //   '^.+\\.(js|jsx)$': 'babel-jest',
+  //   '^.+\\.(ts|tsx)?$': 'ts-jest',
+  // },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+};
