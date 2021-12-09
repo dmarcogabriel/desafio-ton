51e873e3f0ec9a003ba1b49f14a294167be88f19
Gabriel <lino.dmarco@gmail.com>
2021-12-08 23:48:31 -0300

    feat(productList): add infinite list

--- src/pages/ShipsList/ShipsList.tsx
+++ src/pages/ShipsList/ShipsList.tsx
@@ -1,13 +1,17 @@
 import React, {useEffect, useCallback, useState} from 'react';
-import {ShipListContainer} from './styles';
+import styles, {ShipListContainer, ShipListFooter} from './styles';
 import shipListService from './shipList.service';
 import {Ship} from 'src/types';
 import {ShipItem} from './components';
 import {useCart} from '../../hooks';
+import {ActivityIndicator} from 'react-native';
 
 export const ShipsList = (): JSX.Element => {
   const [products, setProducts] = useState<Ship[]>([]);
   const {addProduct, productList, removeProductById} = useCart();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [page, setPage] = useState<number>(1);
+  const [canLoadMore, setCanLoadMore] = useState<boolean>(true);
 
   const handleAddToCart = (ship: Ship) => {
     addProduct(ship);
@@ -21,18 +25,27 @@ export const ShipsList = (): JSX.Element => {
     productList.some(productItem => productItem.id === product.id);
 
   const loadProductsFromService = useCallback(async () => {
-    const data = await shipListService.loadProducts();
-    setProducts(data.results);
-  }, []);
+    if (!isLoading && canLoadMore) {
+      setIsLoading(true);
+      const data = await shipListService.loadProducts(page);
+      setProducts(oldProducts => [...oldProducts, ...data.results]);
+      setCanLoadMore(!!data.next);
+      setPage(page + 1);
+      setIsLoading(false);
+    }
+  }, [isLoading, page, canLoadMore]);
 
   useEffect(() => {
     loadProductsFromService();
-  }, [loadProductsFromService]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <ShipListContainer
       data={products}
-      keyExtractor={item => item.id}
+      keyExtractor={(_, i) => `${i + 1}`}
+      numColumns={2}
+      contentContainerStyle={styles.listContainer}
       renderItem={({item}) => (
         <ShipItem
           ship={item}
@@ -41,6 +54,13 @@ export const ShipsList = (): JSX.Element => {
           onRemoveFromCart={handleRemoveFromCart}
         />
       )}
+      ListFooterComponent={
+        <ShipListFooter>
+          {isLoading && <ActivityIndicator size={24} />}
+        </ShipListFooter>
+      }
+      onEndReachedThreshold={0.1}
+      onEndReached={() => loadProductsFromService()}
     />
   );
 };
--- src/pages/ShipsList/styles.ts
+++ src/pages/ShipsList/styles.ts
@@ -1,6 +1,21 @@
-import {FlatList} from 'react-native';
+import {FlatList, StyleSheet} from 'react-native';
 import styled from 'styled-components/native';
 
 export const ShipListContainer = styled.FlatList`
   flex: 1;
+  background-color: #fff;
 ` as unknown as typeof FlatList;
+
+export const ShipListFooter = styled.View`
+  padding: 16px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default StyleSheet.create({
+  listContainer: {
+    paddingVertical: 8,
+    paddingHorizontal: 8,
+    alignItems: 'center',
+  },
+});
