73983d5bca0e5aa8351997cde63464c47729812e
Gabriel <lino.dmarco@gmail.com>
2021-12-08 23:53:11 -0300

    chore: change status-bar styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {Navigation} from './Navigation';
 import {CartProvider} from './contexts';
+import {StatusBar} from 'react-native';
 
 export const App = (): JSX.Element => (
-  <CartProvider>
-    <NavigationContainer>
-      <Navigation />
-    </NavigationContainer>
-  </CartProvider>
+  <>
+    <StatusBar backgroundColor="#fff" barStyle="dark-content" />
+    <CartProvider products={[]}>
+      <NavigationContainer>
+        <Navigation />
+      </NavigationContainer>
+    </CartProvider>
+  </>
 );
